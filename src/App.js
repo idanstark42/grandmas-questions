c8af38ecf6f19e85e4aa06909d305fb1d3912641
Idan Stark <idanstark42@gmail.com>
2024-09-22 23:38:49 +0300

    Midnight. You come to pick me up no headlights.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import { Icon, Divider, Segment, Progress } from 'semantic-ui-react'
+import { Divider, Icon } from 'semantic-ui-react'
 import Cookie from 'js-cookie'
-import './App.css';
+import Loader from 'react-loaders'
 
 const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbz_lJ4-IMPjSB-y94Vw9bM93CQgvuZ0_Sl9077s2ew8BYwGMCOL8J_GF0P-WGSHzkxi/exec?action=load'
 
@@ -12,12 +12,15 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [showingAnswer, setShowingAnswer] = useState(false)
   const [responses, setResponses] = useState([])
+  const [animate, setAnimate] = useState(true)
 
   // manage responses in cookies
   
   const loadResponses = () => {
     const savedResponses = JSON.parse(Cookie.get('responses') || '[]')
     setResponses(savedResponses)
+    console.log('here')
+    setShowingAnswer(savedResponses[currentQuestion] !== undefined)
   }
 
   const saveResponses = (newResponses) => {
@@ -43,8 +46,10 @@ function App() {
   }, [])
 
   const goTo = (index) => {
+    setAnimate(false)
     setCurrentQuestion(index)
-    setShowingAnswer(false)
+    setShowingAnswer(responses[index] !== undefined)
+    setTimeout(() => setAnimate(true), 500)
   }
 
   const restart = () => {
@@ -56,12 +61,17 @@ function App() {
     setLoading(false)
   }
 
-  const toggleAnswer = () => setShowingAnswer(!showingAnswer)
+  const toggleAnswer = () => {
+    setAnimate(true)
+    setShowingAnswer(!showingAnswer)
+  }
 
   if (loading) {
-    return <div className='center aligned' style={{ backgroundColor: '#AAAAAA', height: '100%', display: 'flex', flexDirection: 'column', padding: '1rem', gap: '1rem' }}>
-      <div className='ui active dimmer'>
-        <div className='ui text loader'>Loading</div>
+    return <div style={{ height: '100%', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Loader type='ball-scale' active />
+      </div>
+      <div style={{ height: '0%', display: 'flex', justifyContent: 'center', background: 'rgb(204, 204, 204)' }}>
       </div>
     </div>
   }
@@ -72,49 +82,41 @@ function App() {
     const incorrect = responses.filter(response => response === false).length
     const resultMessage = (results.find(result => result.score === correct) || results.find(result => result.score === 'default')).message
 
-    return <div className='center aligned' style={{ backgroundColor: '#AAAAAA', height: '100%', display: 'flex', flexDirection: 'column', padding: '1rem', gap: '1rem' }}>
-      <h1 style={{ textAlign: 'center' }}>Results</h1>
-      <Segment raised style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <h3>You got</h3>
-          <h1 style={{ backgroundColor: '#2185d0', color: 'white', padding: '1rem', borderRadius: '50%', aspectRatio: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 0  }}>{correct}/{correct + incorrect}</h1>
-          <h3>questions correct</h3>
-          <h4>{resultMessage}</h4>
+    return <div style={{ height: '100%', display: 'flex', flexDirection: 'column', gap: '1rem', position: 'relative' }}>
+    <div style={{ height: '70%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '0.2rem' }}>
+          <h3 style={{ margin: 0 }}>צדקת ב</h3>
+          <h1 style={{ color: 'rgb(71, 190, 199)', display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 0  }}>{correct}/{correct + incorrect}</h1>
+          <h3 style={{ margin: 0 }}>שאלות</h3>
         </div>
-      </Segment>
-      <Segment raised style={{ margin: '-1rem', borderRadius: 0 }}>
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '0 0 1rem 0' }}>
-          <button className='ui huge primary circular fluid button' onClick={() => restart()}>Restart</button>
-        </div>
-      </Segment>
+        <h3>{resultMessage}</h3>
+      </div>
+      <div style={{ height: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', padding: '1rem', position: 'relative' }}>
+        <button className='ui huge fluid button' style={{ height: '4rem', width: '100%', margin: 0 }} onClick={restart}>אתחל</button>
+      </div>
     </div>
   }
 
-  return <div className='center aligned' style={{ backgroundColor: '#AAAAAA', height: '100%', display: 'flex', flexDirection: 'column', padding: '1rem', gap: '1rem' }}>
-    <h1 style={{ textAlign: 'center', margin: 0 }}>Grandma's Questions</h1>
-    <Segment raised>
-      <h3 style={{ textAlign: 'center' }}>{questions[currentQuestion].question}</h3>
-      <Divider />
-      {(showingAnswer || responses[currentQuestion] !== undefined) ? <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h4>{questions[currentQuestion].answer}</h4>
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
-          <button className='ui green icon button' onClick={() => respond(currentQuestion, true)} disabled={responses[currentQuestion] !== undefined}><Icon name='check' />Correct</button>
-          <button className='ui red icon button' onClick={() => respond(currentQuestion, false)} disabled={responses[currentQuestion] !== undefined}><Icon name='close' />Incorrect</button>
+  return <div style={{ height: '100%', display: 'flex', flexDirection: 'column', gap: '1rem', position: 'relative' }}>
+    <div style={{ height: '70%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '2rem' }}>
+      <h4 style={{ textAlign: 'center', color: 'rgb(71, 190, 199)', margin: 0 }}>{currentQuestion + 1}/{questions.length}</h4>
+      <h2 style={{ textAlign: 'center' }}>{questions[currentQuestion].question}</h2>
+    </div>
+    <div style={{ height: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', padding: '1rem', position: 'relative' }}>
+      <button className='ui huge fluid button' style={{ height: '4rem', width: '100%', margin: 0 }} onClick={toggleAnswer}>הצג תשובה</button>
+      <div style={{ height: showingAnswer ? '100%' : 0, color: '#061c30', background: (responses[currentQuestion] === undefined) ? '#cccccc' : ((responses[currentQuestion] === true) ? '#21ba45' : '#ca1010'), position: 'absolute', bottom: 0, left: 0, width: '100%', transition: (animate && showingAnswer) ? 'height ease-in-out 0.7s' : '', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '1rem', overflow: 'hidden' }}>
+        <h3 style={{ textAlign: 'center', margin: '0 1rem' }}>{questions[currentQuestion].answer}</h3>
+        <h4 style={{ textAlign: 'center', margin: 0 }}>צדקת?</h4>
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'center', gap: '2rem' }}>
+          <button className={`ui large circular icon${(responses[currentQuestion] === true) ? '' : ' basic'} green button`} style={(responses[currentQuestion] === true) ? { border: '1px solid white' } : {}} onClick={() => respond(currentQuestion, true)}><Icon name='check' /></button>
+          <button className={`ui large circular icon${(responses[currentQuestion] === false) ? '' : ' basic'} red button`} style={(responses[currentQuestion] === false) ? { border: '1px solid white' } : {}} onClick={() => respond(currentQuestion, false)}><Icon name='close' /></button>
         </div>
-      </div> : ''}
-    </Segment>
-    <div style={{ flexGrow: 1 }} ></div>
-    <Segment raised style={{ margin: '-1rem', borderRadius: 0 }}>
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '0 0 1rem 0' }}>
-        <button className='ui huge primary circular fluid button' onClick={toggleAnswer} disabled={responses[currentQuestion] !== undefined}>{showingAnswer ? 'Hide' : 'Show'} Answer</button>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <button className='ui basic black icon button' onClick={() => goTo(currentQuestion - 1)} disabled={currentQuestion === 0}><Icon name='arrow left' />Previous</button>
-        <div>{responses.filter(response => response).length}/{questions.length}</div>
-        <button className='ui basic black icon button' onClick={() => goTo(currentQuestion + 1)} disabled={responses[currentQuestion] === undefined}><Icon name='arrow right' />Next</button>
-      </div>
-      <Progress size='tiny' color='blue' percent={100 * responses.filter(response => response !== undefined).length / questions.length} style={{ margin: '1rem 0 0 0' }}/>
-    </Segment>
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'space-between', position: 'absolute', width: '100%', top: '50%', height: '3rem', padding: '0 1rem' }}>
+      <button className='ui circular inverted icon button' style={{ aspectRatio: 1 }} onClick={() => goTo(currentQuestion - 1)} disabled={currentQuestion === 0}><Icon name='arrow right' /></button>
+      <button className='ui circular inverted icon button' style={{ aspectRatio: 1 }} onClick={() => goTo(currentQuestion + 1)} disabled={responses[currentQuestion] === undefined}><Icon name='arrow left' /></button>
+    </div>
   </div>
 }
 
